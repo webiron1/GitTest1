68eedead1365ceff6bb3dc41067d3cce2de25817
Doug Ashbaugh <doug.ashbaugh@outlook.com>
2015-08-27 12:40:02 -0400

    Update index.php

--- GitTest1a/index.php
+++ GitTest1a/index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>This is a Git Commit test!</title>
+        <title>This is a Git Commit test! Test Update.</title>
     </head>
     <body>
         <?php
